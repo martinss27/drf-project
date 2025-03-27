ec95ca977bf05dcf2e2cffe43e9364bb6eeff768
martinss27 <leoalmeidamartins@gmail.com>
2025-03-26 22:07:14 -0300

    Added 'ECHO GET DATA' functionality

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,21 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "hi there, this is your django rest framework API response"})
+    # request _> HttpRequest -> Django
+    #print(dir(request))
+    #request.body
+    print(request.GET) #query params
+    body = request.body #byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) #string of JSON data -> Python Dict
+    except:
+        pass
+    print(data)
+    data ['headers'] = dict(request.headers) # request.META ->
+    data ['content_type'] = request.content_type
+    data ['params'] = dict(request.GET)
+
+    return JsonResponse(data)
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,12 +4,12 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #http://localhost:8000/api/
 
-get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"}) # HTTP Request
 
-print(get_response.text) # print raw text response
-print(get_response.status_code)
+#print(get_response.text) # print raw text response
+#print(get_response.status_code)
 
-print(get_response.json()['message'])
+print(get_response.json())
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
