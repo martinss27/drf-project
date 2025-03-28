5a81d4134c9f6d4099d553f1dfd849ea5b55e024
martinss27 <leoalmeidamartins@gmail.com>
2025-03-28 14:19:32 -0300

    Add example models for API with data conversion to Python dictionary and JSON response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,18 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
-def api_home(request, *args, **kwargs):
+def api_home(request, *args, **kwargs): #*args, **kwargs -> argumentos posicionais e argumentos nomeados
     # request _> HttpRequest -> Django
     #print(dir(request))
     #request.body
-    print(request.GET) #query params
-    body = request.body #byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) #string of JSON data -> Python Dict
-    except:
-        pass
-    print(data)
-    data ['headers'] = dict(request.headers) # request.META ->
-    data ['content_type'] = request.content_type
-    data ['params'] = dict(request.GET)
-
+    
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price    
     return JsonResponse(data)
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #http://localhost:8000/api/
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"}) # HTTP Request
+get_response = requests.get(endpoint, json={"product_id": 123}) # HTTP Request
 
 #print(get_response.text) # print raw text response
 #print(get_response.status_code)
