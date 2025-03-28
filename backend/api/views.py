874d7c2bcc057f53bbd63c32f133301e01f4d3c9
martinss27 <leoalmeidamartins@gmail.com>
2025-03-28 17:54:49 -0300

    implement serializers and forms for better data handling

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,15 +2,18 @@ import json
 from django.http import JsonResponse
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
-def api_home(request, *args, **kwargs): #*args, **kwargs -> argumentos posicionais e argumentos nomeados
-    # request _> HttpRequest -> Django
-    #print(dir(request))
-    #request.body
-    model_data = Product.objects.all().order_by("?").first()
+@api_view(['GET', 'POST'])
+def api_home(request, *args, **kwargs):
+
+    instance = Product.objects.all().order_by("?").first()
     data = {}
     
-    if model_data:
-        data = model_to_dict(model_data)  
+    if instance:
+       # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])  
+       data = ProductSerializer(instance).data
     return JsonResponse(data)
 
