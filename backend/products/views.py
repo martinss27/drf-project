26198643e973eb9bc19e6c93a54de1000236d3bb
martinss27 <leoalmeidamartins@gmail.com>
2025-04-01 14:59:17 -0300

    Implementing ListAPIView & CreateAPIView together (READ & CREATE)

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,19 +3,19 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def perform_create(self, serializer):
-        #serializer.save(user=self.requesr.user)
+        #serializer.save(user=self.request.user)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None  
         if content is None:
             content = title
         serializer.save(content=content)
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
